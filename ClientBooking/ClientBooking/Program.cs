6e90d8f6b8d5b272d6b143930a04713e62439c4b
Josh Morton <josh.morton@propeltech.co.uk>
2025-11-13 19:19:43 +0000

    lay framework for razor component rendering, add generic docker files

--- ClientBooking/ClientBooking/Program.cs
+++ ClientBooking/ClientBooking/Program.cs
@@ -1,11 +1,37 @@
 global using Microsoft.AspNetCore.Http.HttpResults;
 global using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Hosting.StaticWebAssets;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Configuration.AddEnvironmentVariables();
+
+if (builder.Environment.IsProduction())
+{
+}
+else
+{
+    StaticWebAssetsLoader.UseStaticWebAssets(builder.Environment, builder.Configuration);
+}
+
+builder.Services.AddAntiforgery();
+
+builder.Services.AddRazorComponents();
+
 var app = builder.Build();
 
-app.UseHttpsRedirection();
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseHsts();
+}
 
+//Add Migrations here
+
+app.MapStaticAssets();
+
+app.UseAntiforgery();
+
+//MapRazorComponents Here
 
 app.Run();
