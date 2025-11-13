51543b85a514c30a865e44fdadcb98bc0eb7bef8
Josh Morton <josh.morton@propeltech.co.uk>
2025-11-13 20:16:29 +0000

    Configure DataContext and explicitly state relationships for all EF entities

--- ClientBooking/Data/DataContext.cs
+++ ClientBooking/Data/DataContext.cs
@@ -1,9 +1,21 @@
-﻿namespace ClientBooking.Data;
+﻿using ClientBooking.Data.Entities;
+using ClientBooking.Data.JoiningTables;
+
+namespace ClientBooking.Data;
 
 public class DataContext(DbContextOptions<DataContext> options) : DbContext(options)
 {
-    protected override void OnModelCreating(ModelBuilder modelBuilder) { }
-
+    public DbSet<Settings> Settings { get; set; }
+    public DbSet<Client> Clients { get; set; }
+    public DbSet<Booking> Bookings { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<Role> Roles { get; set; }
+    public DbSet<UserUnavailability> UserUnavailabilities { get; set; }
+    public DbSet<Notification> Notifications { get; set; }
+    
+    public DbSet<UserBooking> UserBookings { get; set; }
+    public DbSet<UserRole> UserRoles { get; set; }
+    
     public override Task<int> SaveChangesAsync(CancellationToken ct = new())
     {
         foreach (var entry in ChangeTracker.Entries())
@@ -16,5 +28,107 @@ public class DataContext(DbContextOptions<DataContext> options) : DbContext(opti
         }
 
         return base.SaveChangesAsync(ct);
+    }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        //Composite Keys
+        modelBuilder.Entity<UserBooking>().HasKey(ub => new { ub.BookingId, ub.UserId });
+        modelBuilder.Entity<UserRole>().HasKey(ur => new { ur.UserId, ur.RoleId });
+
+        //Use RowVersion has concurrency provider
+        modelBuilder.Entity<Entity>()
+            .Property(e => e.RowVersion)
+            .IsConcurrencyToken();
+        
+        //Entity relationships
+        modelBuilder.Entity<Booking>()
+            .HasOne(b => b.Client)
+            .WithMany(c => c.Bookings)
+            .HasForeignKey(b => b.ClientId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<UserBooking>()
+            .HasOne(ub => ub.Booking)
+            .WithMany(b => b.UserBookings)
+            .HasForeignKey(ub => ub.BookingId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<UserBooking>()
+            .HasOne(ub => ub.User)
+            .WithMany(u => u.UserBookings)
+            .HasForeignKey(ub => ub.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<UserRole>()
+            .HasOne(ur => ur.User)
+            .WithMany(u => u.UserRoles)
+            .HasForeignKey(ur => ur.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<UserRole>()
+            .HasOne(ur => ur.Role)
+            .WithMany(r => r.UserRoles)
+            .HasForeignKey(ur => ur.RoleId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<UserUnavailability>()
+            .HasOne(u => u.User)
+            .WithMany(x => x.UnavailabilityPeriods)
+            .HasForeignKey(u => u.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<Notification>()
+            .HasOne(n => n.User)
+            .WithMany(u => u.Notifications)
+            .HasForeignKey(n => n.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        
+        //Configure deletion behaviour for related entities.
+        modelBuilder.Entity<Notification>()
+            .HasOne(n => n.Booking)
+            .WithMany(b => b.Notifications)
+            .HasForeignKey(n => n.BookingId)
+            .OnDelete(DeleteBehavior.NoAction);
+
+        modelBuilder.Entity<Settings>()
+            .HasOne(s => s.SavedBy)
+            .WithMany()
+            .HasForeignKey(s => s.SavedById)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<Client>()
+            .HasOne(c => c.SavedBy)
+            .WithMany()
+            .HasForeignKey(c => c.SavedById)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<Booking>()
+            .HasOne(b => b.SavedBy)
+            .WithMany()
+            .HasForeignKey(b => b.SavedById)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<Role>()
+            .HasOne(r => r.SavedBy)
+            .WithMany()
+            .HasForeignKey(r => r.SavedById)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<UserUnavailability>()
+            .HasOne(u => u.SavedBy)
+            .WithMany()
+            .HasForeignKey(u => u.SavedById)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<Notification>()
+            .HasOne(n => n.SavedBy)
+            .WithMany()
+            .HasForeignKey(n => n.SavedById)
+            .OnDelete(DeleteBehavior.Restrict);
+    
     }
 }
