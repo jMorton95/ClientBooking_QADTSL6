e89e52c62f922b1696380f013072773834661c13
Joshua Morton <mortson@hotmail.co.uk>
2025-12-12 10:38:19 +0000

    [fix] Filter audit logs down more

--- ClientBooking/Features/Audit/AuditHandler.cs
+++ ClientBooking/Features/Audit/AuditHandler.cs
@@ -39,6 +39,7 @@ public class AuditHandler : IRequestHandler
         
         var auditLogs = await dataContext.AuditLogs
             .OrderByDescending(x => x.Id)
+            .Where(al => al.UserId != null)
             .Skip((pageNumber - 1) * 60)
             .Take(60)
             .ToListAsync();
